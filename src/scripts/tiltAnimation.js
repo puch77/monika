37d96e3a433b8a9ff4b433b8b28a89ebfc595af4
MoniPuch <123469298+puch77@users.noreply.github.com>
2024-06-20 23:17:33 +0200

    a
    b

--- src/scripts/tiltAnimation.js
+++ src/scripts/tiltAnimation.js
@@ -1,4 +1,4 @@
-import VanillaTilt from "../../node_modules/vanilla-tilt/dist/vanilla-tilt.js";
+import VanillaTilt from "../../node_modules/vanilla-tilt";
 
 export default function initTiltAnimation() {
   const elements = document.querySelectorAll(".js-tilt");
